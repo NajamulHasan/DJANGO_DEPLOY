81d841537a7dd0739f1575978dedc416ba1a52db
najam <nulhasan84@gmail.com>
2024-01-08 12:55:47 +0000

    Nine  Commit

--- demo/wsgi.py
+++ demo/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module='Demo.deployment' if 'WEBsITE_HOSTNAME' in os.environ else 'Demo.settings'
+settings_module='Demo.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'Demo.settings'
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
