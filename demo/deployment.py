b340ead05357fa6fce846eeba015cd19b842d0fa
najam <nulhasan84@gmail.com>
2024-01-10 21:14:05 +0000

    NewCommit5

--- demo/deployment.py
+++ demo/deployment.py
@@ -23,9 +23,10 @@ STATICFILES_DIRS = [
 ]
 
 
-
+STATIC_URL='/static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
