f905e889c32912595af77b6777eb48388c762771
najam <nulhasan84@gmail.com>
2024-01-10 20:36:48 +0000

    NewCommit

--- demo/deployment.py
+++ demo/deployment.py
@@ -2,10 +2,9 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTs=[os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS=['http://'+ os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]  # Fixed typo in ALLOWED_HOSTS
+CSRF_TRUSTED_ORIGINS = ['http://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 
 MIDDLEWARE = [
@@ -19,17 +18,21 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFileStorage'
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Corrected the path
+]
 
+STATIC_URL = "/static/"  # Fixed STATIC_URL
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ['AZURE_MYSQL_NAME'],
         'HOST': os.environ['AZURE_MYSQL_HOST'],
-        'USER': os.environ['AZURE_MYSQL_USER'],  
+        'USER': os.environ['AZURE_MYSQL_USER'],
         'PASSWORD': os.environ['AZURE_MYSQL_PASSWORD'],
     }
 }
